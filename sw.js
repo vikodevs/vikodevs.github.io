adb62e5c43e0d8f2065859adba64208565975d06
Lucky-victory <blizwonder25@gmail.com>
2021-02-05 13:53:05 +0100

    Testing PWA

---
+++ sw.js
@@ -0,0 +1,17 @@
+let cacheName="hello-pwa";
+let filesToCache=['/','/index.html','/css/style.css','/js/script.js'];
+self.addEventListener("install",function(e){
+  e.waitUntil(
+    catches.open(cacheName).then(function(cache){
+      return cache.addAll(filesToCache)
+    })
+    );
+});
+self.addEventListener("fetch",function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(response){
+      return response || fetch(e.request)
+    })
+    );
+});
+console.log(window)
